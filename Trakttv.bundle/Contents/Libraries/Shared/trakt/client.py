0050ee0746b6d0ff5d2227271351c8807645689e
MCs88 <mezei.csaba@gmail.com>
2016-05-12 23:15:37 +0200

    Improve resiliency of specific network errors
    Improve resiliency of specific network errors

--- Trakttv.bundle/Contents/Libraries/Shared/trakt/client.py
+++ Trakttv.bundle/Contents/Libraries/Shared/trakt/client.py
@@ -17,6 +17,7 @@ class TraktClient(Emitter):
         if adapter_kwargs is None:
             adapter_kwargs = {}
 
+        adapter_kwargs.setdefault('max_retries', 10)
 
         # Construct
         self.configuration = ConfigurationManager()
